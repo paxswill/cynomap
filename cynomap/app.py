f2741be8e94a340d49059b15c2764ede5f026c39
Will Ross <paxswill@gmail.com>
2014-07-10 20:06:31 -0400

    Add Jquery tooltips and a full-size map
    It looks pretty

--- cynomap/app.py
+++ cynomap/app.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 from cynomap import CynoMap
 
 app = Flask(__name__)
@@ -8,14 +8,18 @@ app = Flask(__name__)
 @app.route('/cynos.svg')
 @app.route('/cynos-<range>.svg')
 def cynos(range=13):
-	logging.info('Range %s' % range)
-	map = CynoMap(jumprange=float(range), keyid=os.environ['CYNOMAP_KEYID'], vcode=os.environ['CYNOMAP_VCODE']).svg.standalone_xml()
-	return Response(mimetype='image/svg+xml', response=map)
+    logging.info('Range %s' % range)
+    map = CynoMap(jumprange=float(range), keyid=os.environ['CYNOMAP_KEYID'], vcode=os.environ['CYNOMAP_VCODE']).svg.standalone_xml()
+    return Response(mimetype='image/svg+xml', response=map)
 
 @app.route('/')
-def index():
-	return """<iframe src="/cynos.svg" width="600px" height="600px" scrolling="yes"></iframe>"""	
-	
+@app.route('/<float:jump_range>/')
+def index(jump_range=13.0):
+    svg = CynoMap(jumprange=jump_range,
+            keyid=os.environ['CYNOMAP_KEYID'],
+            vcode=os.environ['CYNOMAP_VCODE']).svg.xml()
+    return render_template('index.html', mapsvg=svg)
+
 if __name__ == '__main__':
-	logging.basicConfig(level=logging.DEBUG)
-	app.run(host='0.0.0.0')
+    logging.basicConfig(level=logging.DEBUG)
+    app.run(host='0.0.0.0')
